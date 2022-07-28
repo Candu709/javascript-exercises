956b449e6d82e54223c987673b0a102ee5afbaf1
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-07-28 23:19:31 +0200

    Complete exercise 01 to 04
    Few commit since these are simple exercises
    In fact only pushing to move from Laptop to Desktop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reverse = [];
+  for (i=0; i < string.length; i++) {
+    reverse[i] = string[string.length-i-1];
+  };
+  return reverse.join('');
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(original,...targets) {
+  let filtered = original;
+  for (const target of targets) {
+    filtered = filtered.filter(element => element !== target)
+  }
+  return filtered
 };
 
 // Do not edit below this line
